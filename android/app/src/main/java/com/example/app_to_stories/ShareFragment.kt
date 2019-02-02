50355e7690dc1a065477ccfa033d0557b3d8e14e
William FK <williamferk@gmail.com>
2019-02-02 13:24:26 +0100

    android - stories sharing

--- android/app/src/main/java/com/example/app_to_stories/ShareFragment.kt
+++ android/app/src/main/java/com/example/app_to_stories/ShareFragment.kt
@@ -1,20 +1,28 @@
 package com.example.app_to_stories
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_share.*
+import java.io.FileNotFoundException
 
 class ShareFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
     }
 
     override fun onCreateView(
@@ -32,26 +40,33 @@ class ShareFragment : Fragment() {
 
     private fun shareContent() {
         Log.d("Tag", "sharing content")
-        // Define image asset URI and attribution link URL
-        val backgroundAssetUri = Uri.parse("https://sa.kapamilya.com/absnews/abscbnnews/media/2018/news/10/16/20181016_bolt.jpg")
-        //String attributionLinkUrl = "https://www.my-aweseome-app.com/p/BhzbIOUBval/";
+        val intent = activity!!.packageManager.getLaunchIntentForPackage("com.instagram.android")
+        val shareIntent = Intent("com.instagram.share.ADD_TO_STORY")
+        //shareIntent.setPackage("com.instagram.android")
 
-        // Instantiate implicit intent with ADD_TO_STORY action,
-        // background asset, and attribution link
-        val intent = Intent("com.instagram.share.ADD_TO_STORY")
-        //intent.setType("image/*")
+        val drawable = ContextCompat.getDrawable(activity!!, R.drawable.liverpool)
+        Log.d("FKZ", "drawable: $drawable")
 
-        intent.setDataAndType(backgroundAssetUri, "image/*")
-        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-        intent.putExtra("top_background_color", "#33FF33")
-        intent.putExtra("bottom_background_color", "#FF00FF")
+        val bitmap = (drawable as? BitmapDrawable)?.bitmap
+        Log.d("FKZ", "bitmap: $bitmap")
 
-        // Instantiate activity and verify it will resolve implicit intent
-        //Activity activity = getActivity();
-        if (activity!!.packageManager.resolveActivity(intent, 0) != null) {
-            Log.d("Tag", "resolved")
-            activity!!.startActivity(intent)
-            Log.d("Tag", "resolved2")
-        }
+        val path = MediaStore.Images.Media.insertImage(
+            activity!!.contentResolver,
+            bitmap,
+            "Liverpool.jpg",
+            "You will never walk alone"
+        )
+        Log.d("FKZ", "path: $path")
+
+        val uri = Uri.parse(path)
+
+        shareIntent.setDataAndType(uri, "image/*")
+
+
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+        activity!!.grantUriPermission("com.instagram.android", uri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        startActivityForResult(shareIntent, 42)
+
     }
 }
